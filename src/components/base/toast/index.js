20842d2b90de9e2bd0714cba71b617a313889d84
likaixuan <1375502718@qq.com>
2017-07-07 23:37:32 +0800

    优化组件代码、在toast自身添加open方法（删除toast方法、this.toast不变）

--- src/components/base/toast/index.js
+++ src/components/base/toast/index.js
@@ -14,31 +14,30 @@ zfToast.install = function (Vue) {
   obj = new (Vue.extend(zfToast))({
     el: document.createElement('div'),
   });
- 
 
-   Vue.prototype.toast = zfToast.toast = (options) => {
+
+  Vue.prototype.toast = zfToast.open = (options) => {
     clearTimeout(timeID);
-  
-    if (!options) {
-      options = {};
-    }
+    options = options || {};
+    
     if (!options.text) {
       return;
     }
     options.timer = options.timer || 3000;
-  
-    if (!options.location) {
-      options.location = "center"
+
+    if(['top', 'center','bottom'].indexOf(options.location) ===-1) {
+        options.location = "center"
     }
+
     obj.text = options.text;
     obj.location = options.location;
     obj.isShow = true;
-    timeID = setTimeout(()=>{
-         obj.isShow = false;
-    },options.timer);
+    timeID = setTimeout(() => {
+      obj.isShow = false;
+    }, options.timer);
 
   }
- 
+
   document.body.appendChild(obj.$el);
 };
 
--- src/components/base/toast/src/toast.vue
+++ src/components/base/toast/src/toast.vue
@@ -19,27 +19,5 @@
     };
 </script>
 <style lang="scss" scoped>
-    .toast {
-        position:fixed;
-        left:50%;
-        transform:translate(-50%,-50%);
-        padding:8px;
-        border-radius:3px;
-        background:rgba(0,0,0,0.5);
-        box-sizing: content-box;
-        color:white;
-        font-size:16px;
-        z-index:1100;
-        max-width:200px;
-    }
-    .top {
-        top:20%;
-    }
-    .center {
-        top:50%;
-
-    }
-    .bottom {
-        bottom:20%;
-    }
+   
 </style>
