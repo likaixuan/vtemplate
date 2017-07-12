3bcd41b47e844e4c11bc090a4d662c2883c83282
likaixuan <1375502718@qq.com>
2017-07-12 14:04:19 +0800

    添加最大宽度

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,46 @@
 <template>
   <div id="app">
-    <keep-alive>
-      <router-view></router-view>
-    </keep-alive>
+    <transition name="Jump">
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive>
+    </transition>
   </div>
 </template>
 
 <script>
-  /*
-   *----------------------------
-   * 主布局(layout)
-   *----------------------------
-   * @author likaixuan
-   * @date 2017/05/26
-   * 
-   */
+/*
+ *----------------------------
+ * 主布局(layout)
+ *----------------------------
+ * @author likaixuan
+ * @date 2017/05/26
+ * 
+ */
 
-  export default {
+export default {
 
-    name: 'app',
-    data() {
-      return {
-
-      }
-    },
-    mounted: function () {
+  name: 'app',
+  data() {
+    return {
 
     }
+  },
+  mounted: function () {
+
   }
+}
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+  .Jump-enter-active,
+  .Jump-leave-active {
+    transition: opacity .5s
+  }
 
+  .Jump-enter,
+  .Jump-leave-to
+  {
+    opacity: 0
+  }
 </style>
